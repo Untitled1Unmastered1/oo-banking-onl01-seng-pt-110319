ae099e139267525e74488df65bebcd11ab1a04f5
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-04 06:03:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,15 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount 
+  
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end 
+  
+  def valid? 
+    
+    
+  end 
 end
