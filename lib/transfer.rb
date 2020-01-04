805d25dd9492dcd27b89397eb6fca789a9372944
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-04 06:52:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,15 +14,26 @@ class Transfer
   end 
   
   def execute_transaction
-    if @status = "rejected" && sender.balance < @amount 
-      return "Transaction rejected. Please check your account balance."
-    else 
-       @sender.balance > @amount && @status = "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
+  
+  # def execute_transaction
+  #   if @status = "rejected" && sender.balance < @amount 
+  #     return "Transaction rejected. Please check your account balance."
+  #   else 
+  #     @sender.balance > @amount && @status = "pending"
+  #     @sender.balance -= @amount
+  #     @receiver.balance += @amount
+  #     @status = "complete"
+  #   end
+  # end
 
  def reverse_transfer
     if @status == "complete"
